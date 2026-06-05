a3fe8974ff5ebb3a54e4ae06d93d8021e65c8105
poundskartmanager-gif <poundskart@gmail.com>
2026-06-05 23:23:49 +0100

    Update install.js

--- netlify/functions/install.js
+++ netlify/functions/install.js
@@ -1,1 +1,14 @@
+exports.handler = async (event) => {
+  const shop = 'poundskart.myshopify.com';
+  const clientId = process.env.SHOPIFY_CLIENT_ID;
+  const redirectUri = 'https://thunderous-tulumba-6647ef.netlify.app/.netlify/functions/auth';
+  const scopes = 'write_products,read_products';
 
+  const authUrl = `https://${shop}/admin/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code`;
+
+  return {
+    statusCode: 302,
+    headers: { Location: authUrl },
+    body: ''
+  };
+};
